1b0cbad86755d38343aa72269d708a3d74244e5c
Nabemh <nhanior@gmail.com>
2024-07-09 13:52:29 +0100

    trial 2

--- help/core/admin.py
+++ help/core/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
-from .models import Complaint, CategoryComplaint
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Profile
 
-admin.site.register(Complaint)
-admin.site.register(CategoryComplaint)
+# Register Models here
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (ProfileInline, )
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('email', 'phone_number', 'room_number', 'hostel')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- help/core/models.py
+++ help/core/models.py
@@ -3,44 +3,27 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-<<<<<<< HEAD
 class Profile(models.Model):
-     user = models.OneToOneField("User.Model", verbose_name=_("User"), on_delete=models.CASCADE)
-     room_number = models.CharField(max_length=15, blank=null, null=True)
-     phone_number = models.CharField(max_lenght=100, blank=null, null=True)
+     user = models.OneToOneField("User", verbose_name=_("User"), on_delete=models.CASCADE)
+     room_number = models.CharField(max_length=15, blank=True, null=True)
+     phone_number = models.CharField(max_lenght=20, blank=True, null=True)
+     email = models.EmailField(max_length=100, blank=True, null=True)
      hostel = models.ForeignKey('Hostel', on_delete=models.SET_NULL, null=True)
 
      def __str__(self):
          return self.user.username
 
-=======
-
-class CategoryComplaint(models.Model):
-    cat_name = models.CharField(max_length=80)
-
-    def __str__(self):
-        return self.cat_name
->>>>>>> 225f72df91d0081e76f67753f95f67a628a2480a
 class Complaint(models.Model):
-    MY_CHOICES = (
-        ('1','Old boys Hostel'),
-        ('2','New boys Hostel'),
-        ('3', 'Old Girls Hostel'),
-        ('4', 'New Girls Hostel')
-    )
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=255)
-    category = models.ForeignKey(CategoryComplaint, related_name="complaints", on_delete=models.CASCADE)
-    date_added = models.DateField(auto_now_add=True)
-    hostel = models.CharField(max_length=10, choices=MY_CHOICES, blank=True, null= True)
-    mark_as_done = models.BooleanField(default=False)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        verbose_name_plural = 'Complaints'
+        verbose_name_plural = 'complaints'
         ordering = ('date_added',)
 
     def __str__(self) -> str:
-        return f"{self.name}, {self.hostel}, {str(self.date_added)}"
+        return self.name
     
 
-
+    
