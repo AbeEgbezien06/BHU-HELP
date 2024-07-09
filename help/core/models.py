5fb9a0e1ecc8d0db9fb6f99490b3fa42a4a35d0f
Nabemh <nhanior@gmail.com>
2024-07-09 13:34:54 +0100

    User model created and migrated

--- help/core/models.py
+++ help/core/models.py
@@ -1,6 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+     user = models.OneToOneField("User.Model", verbose_name=_("User"), on_delete=models.CASCADE)
+     room_number = models.CharField(max_length=15, blank=null, null=True)
+     phone_number = models.CharField(max_lenght=100, blank=null, null=True)
+     hostel = models.ForeignKey('Hostel', on_delete=models.SET_NULL, null=True)
+
+     def __str__(self):
+         return self.user.username
 
 class Complaint(models.Model):
     name = models.CharField(max_length=100)
